ff493d85f808f6b49ae1c13b85b60f547b3933a1
John Doe <johndoe@email.com>
2024-10-18 09:21:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import SignIn from './Components/Profile/SignIn'
+
+
+function App() {
+
+
+  return (
+    <>
+      {/* <div>Welcome to new App</div> */}
+      <SignIn/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Profile/SignIn.jsx
@@ -0,0 +1,21 @@
+function SignIn(){
+    return(
+        <>
+        <div>
+            <h1>Sign in</h1>
+            <div>
+                <input type="email" placeholder="Email" />
+                <input type="tpassword" placeholder="Password" />
+                <button>Sign in</button>
+            </div>
+        </div>
+        </>
+    )
+}
+export default SignIn
+
+// Formik
+//https://colorlib.com/wp/wp-content/uploads/sites/2/free-html5-and-css3-login-forms.jpg
+//Submit the value in the console
+//Facebook,Google,GitHub,LinkedIn, normal login <<<<<<<< https://react-icons.github.io/react-icons/ 
+//https://www.pngarts.com/files/5/Lines-Transparent-Background-PNG.png
