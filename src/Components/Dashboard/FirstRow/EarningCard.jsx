36b2d846ffa1eaff96edf9602515f682e00b6536
Fumika Mikami <fumicha.3fan1@gmail.com>
2024-11-20 10:16:11 +0000

    earningCard

--- src/Components/Dashboard/FirstRow/EarningCard.jsx
+++ src/Components/Dashboard/FirstRow/EarningCard.jsx
@@ -5,28 +5,35 @@ import { FaClipboardList } from "react-icons/fa";
 import { IoIosChatbubbles } from "react-icons/io";
 
 function EarningCard({title,totalRevenue,icon,color}) {
+    const Icons = {
+        TfiBag: TfiBag, //if I have this icon, then map to the react icon
+        FaDollarSign: FaDollarSign,
+      };
+    const IconComponent = Icons[icon];
     const borderStyle={
          backgroundColor: "white",
          width:"302px",
-         height:"120px",
+         height:"100px",
          borderLeft:"solid",
          borderLeftWidth:"thick",
          borderColor:color,
     }
-   
+  console.log(Icons[icon])
+  console.log(Icons.icon)
+
     return (
         <>
             <div >
-                <div className=" rounded ms-4 p-4" style={borderStyle}>
+                <div className=" rounded ms-4 p-3" style={borderStyle}>
 
                     <div className="d-flex justify-content-between py-1">
                         <div>
-                            <div style={{ fontSize: "112%",color:color }}>{title}</div>
+                            <div style={{ fontSize: "100%",color:color }}>{title}</div>
                             <div className="fw-bold fs-4"> $ {totalRevenue}</div>
                         </div>
-                        <div  style={{ fontSize: "250%" ,color:"#dddfeb"}}>
+                        <div >
                                                        {/* {React.createElement(icon) } */}
-                                                       {icon}
+                                <IconComponent style={{ fontSize: "230%" ,color:"#dddfeb",marginTop:"25% 0"}}  />       
                         </div>
                     </div>
                 </div>
