e34564b3614035a5ba2350f1ae31871c8c455a55
Fumika Mikami <fumicha.3fan1@gmail.com>
2025-01-30 16:27:18 +0900

    up

--- src/HomePage/SideBar.jsx
+++ src/HomePage/SideBar.jsx
@@ -53,7 +53,7 @@ function SideBar() {
           <div className="sideBarStyle"
             style={{
               width: isSidebarOpen ? "20%" : "6%",
-              padding:isSidebarOpen ? "0% 1.5%":"0",
+              padding:isSidebarOpen ? "0.5% 1.5%":"0.5% 0%",
             }}
              id="sideWidth">
             <Container >
@@ -66,7 +66,7 @@ function SideBar() {
                     right:isSidebarOpen ? "10px" : "0px",
                   }}
                     onClick={()=>setIsSidebarOpen(!isSidebarOpen)}>
-                    <MdKeyboardDoubleArrowRight style={{fontSize:"40px"}}/>
+                    <MdKeyboardDoubleArrowRight style={{fontSize:"45px"}}/>
             </div>
               </div>
                {!isSidebarOpen ?
@@ -74,7 +74,7 @@ function SideBar() {
 
               {/* Student Icon */}
               <div className="row smallSideRow"
-              style={{paddingTop:isSidebarOpen ? "0px" : "15px"}}>
+              style={{paddingTop:isSidebarOpen ? "0%" : "35%"}}>
               <div className="portalIcon mt-1" >
               <FaSchoolFlag/>
               </div>
@@ -97,7 +97,7 @@ function SideBar() {
             onClick={() => setOpen(!open)}
             aria-controls="example-collapse-text"
             aria-expanded={open}
-            className="d-flex align-items-center border justify-content-end border-warning 
+            className="d-flex align-items-center border justify-content-center border-warning 
           componentDiv" 
            >
             
@@ -106,17 +106,19 @@ function SideBar() {
             className="MdGridViewIcon smallSidebarIcon border " style={{fontSize:"50px",color:"white"}} />
               
               { expanded?
-              <IoIosArrowDown  className="text-white  border"/>
-              :
+             
               <IoIosArrowForward className="arrowIcon text-white"
               />
+              :
+              <IoIosArrowDown  className="text-white  border"/>
+             
                   }
           </div>
       </div>
       <Collapse in={open}>
         <div id="example-collapse-text">
        
-             {/* StudentData */}
+      {/* StudentData */}
       <div className="component-icon" onClick={() => { navigate('/studentdata')}}
         style={{cursor:"pointer"}}>
       <PiStudent />
@@ -139,7 +141,8 @@ function SideBar() {
     
       </Collapse>
               {/* Chart */}
-              <div className="row smallSideRow">
+              <div className="row smallSideRow"
+              style={{marginTop:"20px"}}>
               <div className="smallSidebarIcon" onClick={()=>navigate("/chartdata")}
                 style={{marginTop:"20pxgit"}}>
               <FaChartArea />
@@ -178,69 +181,82 @@ function SideBar() {
                 <FaSchoolFlag className="text-end col-lg-2 col-md-2 col-sm-4 studentTitleIcon  fs-1 p-0"/>
                <div className="col-lg-10 col-md-10 col-sm-8 tex-start dashTitle  fs-3">Student Portal</div>
                 </div>
-                <div className="row dashRow row align-items-center text-white" >
+                <div className="row dashRow row text-white" >
                 <AiFillDashboard className="col-3  fs-5" /><div className="col-9 fw-bold" onClick={() => { navigate('/dashboard') }} style={{fontSize:"18px",cursor: "pointer"}} id="dashboard" >Dashboard</div>
                 </div>
 
-        {/* COMPONENT */}
-        <div
-        className="row dashRow Component"
-         onClick={() => setOpen(!open)}  aria-controls="example-collapse-text"  
-         aria-expanded={open}>
-      <IoSettings className="col-3 p-0"/>
-      <div id="component" className="col-7 ps-2">
-      Component</div>
-      <ExpandMore id="expandIcon" className="col-2 p-0"onClick={handleExpandClick} 
-       >    
-      <ExpandMoreIcon className="text-white " aria-expanded={expanded} aria-label="show more" expand={expanded} style={{textAlign:"right"}}/>
-    </ExpandMore>
-       <Collapse in={open}>
-       <div id="example-collapse-text" 
-       style={{borderRadius:"10px", backgroundColor:"white",color:"black",padding:"3%", margin:"2% 10% 0% 10%",width:"220px"
-       }
-       }       >
-        <div className="sidebarItem"   onClick={() => { navigate('/studentdata') }} >
-            View All Student
-        </div>
-        <div className="sidebarItem"  style={{ fontSize: "90%",width:"100%",textAlign:"start",padding:"3% 10%" }} onClick={()=>{navigate('/batchdata')}}>
-            View All Batch
-        </div>
-        <div className="sidebarItem"  style={{ fontSize: "90%",width:"100%",textAlign:"start",padding:"3% 10%" }} onClick={()=>{navigate('/coursedata')}}>
-            View All Course
-        </div>
-        
-        <div className="sidebarItem"
-        style={{fontSize: "90%",width:"100%",textAlign:"start",padding:"3% 10%"}}
-        onClick={()=>{navigate('/admissiondata')}}>
-          View All Admission
-        </div>
-        </div>
-      </Collapse>
-      </div>
+            {/* COMPONENT */}
+            <div 
+            className="dashRow row"
+            onClick={handleExpandClick}
+             >
+          
+            <div
+            id="expandIcon " 
+            onClick={() => setOpen(!open)}
+            aria-controls="example-collapse-text"
+            aria-expanded={open}
+            className="d-flex  align-items-center">
+            
+            <IoSettings className="col-2 "/>
+           <div id="component" className="ps-4 col-8 ">
+            Component</div>
+              
+              {expanded?
+             
+              <IoIosArrowForward className="col-2 fs-4 text-white"
+              />
+              :
+              <IoIosArrowDown  className="col-2 fs-4 text-white"/> }
+               </div>
+                <Collapse in={open}>
+                <div id="example-collapse-text" 
+              style={{borderRadius:"10px", backgroundColor:"white",color:"black", marginTop:"5%",width:"230px",padding:"3% 0",
+               }}>
 
-      <div className="row dashRow ">
-            {/* UTILITIES */}
-         <FiTool className="col-3" />
+              <div className="sidebarItem"   onClick={() => { navigate('/studentdata') }} >
+            View All Student
+             </div>
+             {/* Batch */}
+            <div className="sidebarItem"  style={{ fontSize: "90%",width:"100%",textAlign:"start",padding:"3% 10%" }} onClick={()=>{navigate('/batchdata')}}>
+            View All Batch
+             </div>
+             {/* Course */}
+              <div className="sidebarItem"  style={{ fontSize: "90%",width:"100%",textAlign:"start",padding:"3% 10%" }} onClick={()=>{navigate('/coursedata')}}>
+                  View All Course
+              </div>
+              {/* Admission */}
+              <div className="sidebarItem"
+              style={{fontSize: "90%",width:"100%",textAlign:"start",padding:"3% 10%"}}
+              onClick={()=>{navigate('/admissiondata')}}>
+                View All Admission
+              </div>
+                </div>
+                </Collapse>
+            </div>
+      
+       {/* UTILITIES */}
+        <div className="row dashRow">
+        <FiTool className="col-3" />
          <div className="col-9" id="utilities">Utilities</div>
-      </div>
-      <div className="row dashRow ">
+            </div>
              {/* PAGES */}
-               <FaFolder className="col-3"/>
+            <div className="row dashRow ">               <FaFolder className="col-3"/>
                <div className="col-9" id="page">Page</div>
-      </div>
-      <div className="row dashRow">
+            </div>
+            <div className="row dashRow">
             {/* CHART */}
                 <FaChartArea className="col-3"  />
                 <div  className="col-9" id="chart">Charts</div>
-      </div>
+            </div>
       
-          <div className="row dashRow">
+            <div className="row dashRow">
               {/* Tables */}
                 <FaTable className="col-3"/>
                 <div className="col-9" id="tables">Tables</div>
-          </div>
-          </>
-}
+            </div>
+            </>
+            }
           </Container>
               {/* </div> */}
             </div>
